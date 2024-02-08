46628a43803c66d7523e285d3c7225193232cfc8
mescalambre@student.apc.edu.ph <cbespiritiu@student.apc.edu.ph>
2024-02-08 14:52:02 +0800

    updated week8

---
+++ week8/reference.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>References</title>
+</head>
+<body>
+    <h1>Inspiration for website!</h1>
+    <ol>
+        <li><a href="https://www.youtube.com/watch?v=ldwlOzRvYOU&t=8s" target="_blank">Video Tutorial</a></li>
+        <li><a href="https://www.w3schools.com/howto/howto_js_mobile_navbar.asp" target="_blank">W3Schools Mobile Navbar Tutorial</a></li>
+    </ol>
+</body>
+</html>
