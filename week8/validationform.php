b9534a6eb3537af7627a39378e4e14f27bf79adc
mescalambre@student.apc.edu.ph <cbespiritiu@student.apc.edu.ph>
2024-02-08 15:14:15 +0800

    updated week8

--- week8/validationform.php
+++ week8/validationform.php
@@ -97,3 +97,28 @@ echo $comment;
 echo "<br>";
 echo $gender;
 ?>
+
+<?php
+$servername = "localhost";
+$username = "webprogss221";
+$password = "=latHen97";
+$dbname = "webprogss221";
+ 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+ 
+$sql = "INSERT INTO mescalambre_myguests (name, email, website, comment, gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+ 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
+?>
